3a1426fdb2d0fdda332bad25af7324011ac32a97
JacobBartlett24 <jacobart@ttu.edu>
2023-03-30 19:29:09 -0500

    Produce random album

--- app/routes/game/$accessToken.tsx
+++ app/routes/game/$accessToken.tsx
@@ -1,8 +1,8 @@
-import { Button, Card, CardBody, CardFooter, CardHeader, Divider, Input, Stack, Text } from "@chakra-ui/react";
+import { Box, Button, Card, CardBody, CardFooter, CardHeader, Divider, Input, Stack, Text } from "@chakra-ui/react";
 import type { ActionArgs, LoaderArgs } from "@remix-run/node";
-import { Form, useActionData } from "@remix-run/react";
-import { useState } from "react";
-import { useLoaderData } from "react-router";
+import { Form, useActionData, useLoaderData } from "@remix-run/react";
+import { db } from "~/utils/db.server";
+import type { TopAlbumsGeneral } from "@prisma/client";
 
 type Album = {
   name: string;
@@ -19,47 +19,19 @@ type Artist ={
   name: string;
 }
 
-async function loadRandomAlbum(accessToken: string | undefined){
-  let searchResults: Array<any> = await fetch(`https://api.spotify.com/v1/search?q=year:2020&type=album&limit=50`, {
-    headers: {
-      "Authorization": `Bearer ${accessToken}`,
-      "Content-Type": "application/json"
-    }
-  }).then(res => res.json())
-  .then(data => data.albums.items);
+async function loadRandomAlbum(){
+  let newSearchResults: Array<TopAlbumsGeneral> = await db.topAlbumsGeneral.findMany({
+    take: 500,
+  });
 
-  let newSearchResults: Array<Album> = mapAlbums(searchResults);
-  const randomVinyl: Album = newSearchResults[Math.floor(Math.random() * newSearchResults.length)];
-
+  const randomVinyl: TopAlbumsGeneral = newSearchResults [Math.floor(Math.random() * newSearchResults.length)];
+  
   return randomVinyl;
 }
 
-function mapAlbums(searchResults: any[]) {
-  let newSearchResults: Array<Album> = [];  
-  searchResults.map((album) => {
-    newSearchResults.push({
-      name: album.name,
-      type: album.type,
-      total_tracks: album.total_tracks,
-      release_date: album.release_date,
-      release_date_precision: album.release_date_precision,
-      genres: album.genres,
-      artists: album.artists,
-      popularity: album.popularity
-      } as Album)
-  });
-  return newSearchResults;
-}
+export async function loader({params}: LoaderArgs): Promise<TopAlbumsGeneral>{
+  let album: TopAlbumsGeneral = await loadRandomAlbum();
 
-export async function loader({params}: LoaderArgs): Promise<Album>{
-  let album: Promise<Album> = loadRandomAlbum(params.accessToken);
-  let test = await fetch(`https://api.spotify.com/v1/playlists/4KmcBdDIbHeO0alvCfk2TC?&limit=100`, {
-    headers: {
-      "Authorization": `Bearer ${params.accessToken}`,
-      "Content-Type": "application/json"
-    }
-  }).then(res => res.json())
-  console.log(test)
   return album;
 }
 
@@ -74,43 +46,43 @@ export async function action({request}: ActionArgs){
 
 export default function GameRoute() {
   //let album: Album = useLoaderData();
-  let data = useActionData()
-
-  let guess: number = 0;
-  console.log(guess)
+  let randomAlbum: TopAlbumsGeneral = useLoaderData()
 
   return(
-    <Card w={"500px"} h={"600px"}>
-      <CardHeader>
-      </CardHeader>
-      <CardBody>
-        <Stack spacing={3}>
-          <Text>1</Text>
-        </Stack>
-        <Divider />
-        <Stack spacing={3}>
-          <Text>2</Text>
-        </Stack>
-        <Divider />
-        <Stack>
-          <Text>3</Text>
-        </Stack>
-        <Divider />
-        <Stack>
-          <Text>4</Text>
-        </Stack>
-        <Divider />
-        <Stack>
-          <Text>5</Text>
-        </Stack>
-        <Divider />    
-      </CardBody>
-      <CardFooter>
-        <Form>
-          <Input />
-          <Button type="submit"></Button>
-        </Form>
-      </CardFooter>
-    </Card>
+    // <Card w={"500px"} h={"600px"}>
+    //   <CardHeader>
+    //   </CardHeader>
+    //   <CardBody>
+    //     <Stack spacing={3}>
+    //       <Text>1</Text>
+    //     </Stack>
+    //     <Divider />
+    //     <Stack spacing={3}>
+    //       <Text>2</Text>
+    //     </Stack>
+    //     <Divider />
+    //     <Stack>
+    //       <Text>3</Text>
+    //     </Stack>
+    //     <Divider />
+    //     <Stack>
+    //       <Text>4</Text>
+    //     </Stack>
+    //     <Divider />
+    //     <Stack>
+    //       <Text>5</Text>
+    //     </Stack>
+    //     <Divider />
+    //   </CardBody>
+    //   <CardFooter>
+    //     <Form>
+    //       <Input />
+    //       <Button type="submit"></Button>
+    //     </Form>
+    //   </CardFooter>
+    // </Card>
+    <div>
+      <h1>{randomAlbum.name}</h1>
+    </div>
   )
 }

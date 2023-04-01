4e6305022b17492035c3744cd0654c37e26b6673
JacobBartlett24 <jacobart@ttu.edu>
2023-04-01 15:03:26 -0500

    Change form to fetcher

--- app/routes/game/$accessToken.tsx
+++ app/routes/game/$accessToken.tsx
@@ -1,22 +1,10 @@
-import type { ActionArgs, LoaderArgs } from "@remix-run/node";
-import { Form, useActionData, useLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import type {ActionArgs, LoaderArgs} from "@remix-run/node";
+import { useActionData, useFetcher, useLoaderData } from "@remix-run/react";
 import { db } from "~/utils/db.server";
 import type { TopAlbumsGeneral} from "@prisma/client";
-import { Box, Card, CardHeader, Heading, Text } from "@chakra-ui/react";
-
-type Album = {
-  name: string;
-  type: string;
-  total_tracks: number;
-  release_date: string;
-  genres: Array<string>;
-  artists: Array<Artist>;
-  popularity: number;
-}
-
-type Artist ={
-  name: string;
-}
+import { Box, Button, Card, CardBody, CardFooter, CardHeader, Heading, Input, StackDivider, Text, VStack } from "@chakra-ui/react";
+import { useState } from "react";
 
 async function loadRandomAlbum(){
   let newSearchResults: Array<TopAlbumsGeneral> = await db.topAlbumsGeneral.findMany({
@@ -36,22 +24,27 @@ export async function loader({params}: LoaderArgs){
 }
 
 export async function action({request}: ActionArgs){
-  let formData = await request.formData();
-  let guess: number = formData.get("guessNumber");
-  guess = guess + 1;
-  return {
-    json: { guess }
-  }
+  const guess = (await request.formData()).get("guessValue");
+  return(
+    json({message: guess, correct: true})
+  )
 }
 
 export default function GameRoute() {
   let randomAlbum: TopAlbumsGeneral = useLoaderData()
+  let guessData = useActionData<typeof action>();
+
+  const fetcher = useFetcher();
+
+  const [guess, setGuess] = useState(0);
 
   function obfuscate(albumName: string){
     let obfuscatedAlbumName: string = "";
+    const regex = /\([^)]*\)/g;
+    albumName = albumName.replace(regex, "");
     for(let i = 0; i < albumName.length; i++){
       if(albumName[i] === " "){
-        obfuscatedAlbumName += "";
+        obfuscatedAlbumName += "\xa0\xa0";
       } else {
         obfuscatedAlbumName += " __";
       }
@@ -64,9 +57,41 @@ export default function GameRoute() {
       <Heading>Guess The Album Daily</Heading>
       <Card>
         <CardHeader>
+          <Text>Title:</Text>
           <Text>{obfuscate(randomAlbum.name!)}</Text>
-          <Text>{randomAlbum.name!}</Text>     
         </CardHeader>
+        <CardBody>
+        <VStack
+          divider={<StackDivider borderColor='gray.200' />}
+          spacing={4}
+          align='stretch'
+        >
+          <Box h='40px'>
+            First Track: {randomAlbum.tracks[0]}
+          </Box>
+          <Box h='40px'>
+            Release Date: {randomAlbum.release_date}
+          </Box>
+          <Box h='40px'>
+            Second Track: {randomAlbum.tracks[1]}
+          </Box>
+          <Box h='40px'>
+            Recent Popularity (0-100): {randomAlbum.popularity}
+          </Box>
+          <Box h='40px'>
+            Artist: {randomAlbum.artists[0].name}
+          </Box>
+        </VStack>
+        </CardBody>
+        <CardFooter>
+          <fetcher.Form method="post">
+            <input type="hidden" name="guessNumber" value="0" />
+            <Box display={"flex"} flexDir={"row"}>
+              <Input type="search" name="guessValue"/>
+              <Button type="submit">Guess</Button>
+            </Box>
+          </fetcher.Form>
+        </CardFooter>
       </Card>
     </Box>
   )

9faa20e183fa50e18e3cbe551c67fe4a2c56d2f6
JacobBartlett24 <jacobart@ttu.edu>
2023-04-12 21:41:16 -0500

    Add font and styling to main game

--- app/routes/game/$accessToken.tsx
+++ app/routes/game/$accessToken.tsx
@@ -3,7 +3,7 @@ import type {ActionArgs, LoaderArgs} from "@remix-run/node";
 import { useActionData, useFetcher, useLoaderData } from "@remix-run/react";
 import { db } from "~/utils/db.server";
 import type { TopAlbumsGeneral, DailyAlbum } from "@prisma/client";
-import { Box, Button, Card, CardBody, CardFooter, CardHeader, FormLabel, Heading, Icon, Input, keyframes, Slide, Spinner, StackDivider, Text, useColorMode, useDisclosure, VStack } from "@chakra-ui/react";
+import { Box, Button, Card, CardBody, CardFooter, CardHeader, Flex, FormLabel, Heading, Icon, Input, keyframes, Slide, Spinner, StackDivider, Text, useColorMode, useDisclosure, VStack } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 import SearchRecommendationDropdown from "~/components/SearchRecommendationDropdown";
@@ -11,6 +11,7 @@ import Header from "~/components/Header";
 import { BsMoonStarsFill } from "react-icons/bs";
 import { AiTwotoneFire } from "react-icons/ai";
 import { supabase } from "~/utils/supabase.server";
+import styles from "../../utils/fonts.css"
 
 async function loadRandomAlbum(){
   let newSearchResults: Array<TopAlbumsGeneral> = await db.topAlbumsGeneral.findMany({
@@ -30,7 +31,10 @@ async function getAlbumName(randomAlbumId: number){
   });
   return album!.name;
 }
-  
+ 
+export const links = () => {
+  return [{ rel: "stylesheet", href: styles }];
+};
 
 export async function loader({request}: LoaderArgs){
   let dailyAlbumId: DailyAlbum | null = await db.dailyAlbum.findFirst({});
@@ -140,7 +144,7 @@ export default function GameRoute() {
         title={`Guess The Album Daily`} 
         leftIcon={<Icon transition={"width .25s"} boxSize={6} as={BsMoonStarsFill} onClick={toggleColorMode} _hover={{cursor: "pointer", boxSize: "8"}}/>} 
         rightIcon={<Box display={"flex"} flexDir={"row"}><Text>1</Text><Icon transition={"width .25s"}  boxSize={6} as={AiTwotoneFire} _hover={{cursor: "pointer", boxSize: "8"}}/></Box>}/>
-    <Box>
+    <Box fontFamily={"Inter"}>
       
         <Slide 
           direction='top' 
@@ -177,19 +181,24 @@ export default function GameRoute() {
           align='stretch'
         >
           <Box h={"3rem"} boxShadow="white" bg={colorMode === "light" ? "blackAlpha.400" : "whiteAlpha.300"} borderRadius={"lg"} display={"flex"} flexDir={"row"} alignItems={"center"} pl={"1rem"}>
-            <FormLabel>First Track:</FormLabel> <Text hidden={guessNumber == 0}>{randomAlbum.tracks[0]}</Text>
+            <FormLabel m={"0 2rem 0 0"} display={"flex"} alignContent={"center"} justifyContent={"flex-end"} w={"13.4rem"}>First Track:</FormLabel>
+            <Text hidden={guessNumber == 0}>{randomAlbum.tracks[0]}</Text>
           </Box>
           <Box h={"3rem"} boxShadow="white" bg={colorMode === "light" ? "blackAlpha.400" : "whiteAlpha.300"} borderRadius={"lg"} display={"flex"} flexDir={"row"} alignItems={"center"} pl={"1rem"}>
-            <FormLabel>Release Date:</FormLabel> <Text hidden={guessNumber <= 1}>{randomAlbum.release_date}</Text>
+            <FormLabel m={"0 2rem 0 0"} display={"flex"} alignContent={"flex-end"} justifyContent={"flex-end"} w={"13.4rem"}>Release Date:</FormLabel>
+            <Text hidden={guessNumber <= 1}>{randomAlbum.release_date}</Text>
           </Box>
           <Box h={"3rem"} boxShadow="white" bg={colorMode === "light" ? "blackAlpha.400" : "whiteAlpha.300"} borderRadius={"lg"} display={"flex"} flexDir={"row"} alignItems={"center"} pl={"1rem"}>
-            <FormLabel>Second Track:</FormLabel> <Text hidden={guessNumber <= 2}>{randomAlbum.tracks[1]}</Text>
+            <FormLabel m={"0 2rem 0 0"} display={"flex"} alignContent={"flex-end"} justifyContent={"flex-end"} w={"13.4rem"}>Second Track:</FormLabel> 
+            <Text hidden={guessNumber <= 2}>{randomAlbum.tracks[1]}</Text>
           </Box>
           <Box h={"3rem"} boxShadow="white" bg={colorMode === "light" ? "blackAlpha.400" : "whiteAlpha.300"} borderRadius={"lg"} display={"flex"} flexDir={"row"} alignItems={"center"} pl={"1rem"}>
-            <FormLabel>Recent Popularity (0-100):</FormLabel> <Text hidden={guessNumber <= 3}>{randomAlbum.popularity}</Text>
+            <FormLabel m={"0 2rem 0 0"}>Recent Popularity (0-100):</FormLabel> 
+            <Text hidden={guessNumber <= 3}>{randomAlbum.popularity}</Text>
           </Box>
           <Box h={"3rem"} boxShadow="white" bg={colorMode === "light" ? "blackAlpha.400" : "whiteAlpha.300"} borderRadius={"lg"} display={"flex"} flexDir={"row"} alignItems={"center"} pl={"1rem"}>
-            <FormLabel>Artist:</FormLabel> <Text hidden={guessNumber <= 4}>{randomAlbum.artists[0].name}</Text>
+            <FormLabel m={"0 2rem 0 0"} display={"flex"} alignContent={"flex-end"} justifyContent={"flex-end"} w={"13.4rem"}>Artist:</FormLabel> 
+            <Text hidden={guessNumber <= 4}>{randomAlbum.artists[0].name}</Text>
           </Box>
         </VStack>
         </CardBody>
